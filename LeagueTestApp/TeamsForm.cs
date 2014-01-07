cd628c01b6daf09ef75860ac161767f3ca0811e9
angangg <asklotzy!gmail.com>
2014-01-07 11:19:14 -0800

    added null checks to teamsform

--- LeagueTestApp/TeamsForm.cs
+++ LeagueTestApp/TeamsForm.cs
@@ -44,51 +44,68 @@ namespace LeagueTestApp
 
             summoner = g.LookupSummonerByName(nameTextBox.Text, regionComboBox.SelectedItem.ToString());
 
-            teams = g.GetTeams(summoner.ID);
-
-            for (int i = 0; i < teams.Count(); i++)
+            try
             {
-                DataRow row = table.NewRow();
+                teams = g.GetTeams(summoner.ID);
+            }
+            catch
+            {
 
-                row["Team Name"] = teams[i].name;
-                row["Number of Members"] = teams[i].roster.memberList.Count().ToString();
-
-                int team3v3Wins = 0;
-                int team3v3Losses = 0;
-
-                int team5v5Wins = 0;
-                int team5v5Losses = 0;
-
-                for (int j = 0; j < teams[i].matchHistory.Count(); j++)
+            }
+            finally
+            {
+                for (int i = 0; i < teams.Count(); i++)
                 {
-                    if (teams[i].matchHistory[j].win)
+                    DataRow row = table.NewRow();
+
+                    row["Team Name"] = teams[i].name;
+                    row["Number of Members"] = teams[i].roster.memberList.Count().ToString();
+
+                    int team3v3Wins = 0;
+                    int team3v3Losses = 0;
+
+                    int team5v5Wins = 0;
+                    int team5v5Losses = 0;
+
+                    if (teams[i].matchHistory != null)
                     {
-                        if (teams[i].matchHistory[j].mapId == 41)
+                        for (int j = 0; j < teams[i].matchHistory.Count(); j++)
                         {
-                            team3v3Wins++;
+                            if (teams[i].matchHistory[j].win)
+                            {
+                                if (teams[i].matchHistory[j].mapId == 41)
+                                {
+                                    team3v3Wins++;
+                                }
+                                else
+                                {
+                                    team5v5Wins++;
+                                }
+                            }
+                            else
+                            {
+                                if (teams[i].matchHistory[j].mapId == 41)
+                                {
+                                    team3v3Losses++;
+                                }
+                                else
+                                {
+                                    team5v5Losses++;
+                                }
+                            }
                         }
-                        else
-                        {
-                            team5v5Wins++;
-                        }
+                        row["Total Win/Loss"] = (team3v3Wins + team5v5Wins).ToString() + "/" + (team3v3Losses + team5v5Losses).ToString();
+                        row["3v3 Win/Loss"] = team3v3Wins.ToString() + "/" + team3v3Losses.ToString();
+                        row["5v5 Win/Loss"] = team5v5Wins.ToString() + "/" + team5v5Losses.ToString();
                     }
                     else
                     {
-                        if (teams[i].matchHistory[j].mapId == 41)
-                        {
-                            team3v3Losses++;
-                        }
-                        else
-                        {
-                            team5v5Losses++;
-                        }
+                        row["Total Win/Loss"] = "No Games Played";
+                        row["3v3 Win/Loss"] = "No Games Played";
+                        row["5v5 Win/Loss"] = "No Games Played";
                     }
+                    table.Rows.Add(row);
                 }
-                row["Total Win/Loss"] = (team3v3Wins + team5v5Wins).ToString() + "/" + (team3v3Losses + team5v5Losses).ToString();
-                row["3v3 Win/Loss"] = team3v3Wins.ToString() + "/" + team3v3Losses.ToString();
-                row["5v5 Win/Loss"] = team5v5Wins.ToString() + "/" + team5v5Losses.ToString();
-
-                table.Rows.Add(row);
             }
             teamsInfoDataGrid.DataSource = table;
         }
