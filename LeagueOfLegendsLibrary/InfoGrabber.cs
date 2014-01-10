420a75ab0664ab4d3b5feb5e89807b3c0914d97c
BenV <villalobosb93@gmail.com>
2014-01-10 14:13:35 -0800

    Fixed compiler error
    oops

--- LeagueOfLegendsLibrary/InfoGrabber.cs
+++ LeagueOfLegendsLibrary/InfoGrabber.cs
@@ -181,13 +181,13 @@ namespace LeagueOfLegendsLibrary
         /// <param name="summonerID">The ID of the summoner</param>
         /// <param name="region">The server region to check</param>
         /// <returns></returns>
-        public MasteryPages GetSummonerMasteries(long summonerID, string region)
+        public MasteryPageCollection GetSummonerMasteries(long summonerID, string region)
         {
-            jSerializer = new DataContractJsonSerializer(typeof(MasteryPages));
-            MasteryPages pages = new MasteryPages();
+            jSerializer = new DataContractJsonSerializer(typeof(MasteryPageCollection));
+            MasteryPageCollection pages = new MasteryPageCollection();
             try
             {
-                pages = (MasteryPages)jSerializer.ReadObject(webClient.OpenRead(string.Format("https://prod.api.pvp.net/api/lol/{1}/v1.1/summoner/{0}/masteries?api_key={2}", summonerID, region, LolInfo.APIKEY)));
+                pages = (MasteryPageCollection)jSerializer.ReadObject(webClient.OpenRead(string.Format("https://prod.api.pvp.net/api/lol/{1}/v1.1/summoner/{0}/masteries?api_key={2}", summonerID, region, LolInfo.APIKEY)));
             }
             catch (WebException e)
             {
