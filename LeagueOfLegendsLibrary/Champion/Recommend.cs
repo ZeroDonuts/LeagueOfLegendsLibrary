043480183da214accae477a5da0404cb442fb546
Abdurrahman Alatas <Alatasaf@gmail.com>
2014-09-22 15:59:51 -0700

    Added RecommendedDto

---
+++ LeagueOfLegendsLibrary/Champion/Recommend.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace LeagueOfLegendsLibrary
+{
+    [DataContract(Name="RecommendedDto")]
+    public class Recommend
+    {
+        //missing BlockDto
+
+        [DataMember(Name = "champion")]
+        private string _champion;
+
+        public string Champion
+        {
+            get
+            {
+                return _champion;
+            }
+        }
+
+        [DataMember(Name = "map")]
+        private string _map;
+
+        public string Map
+        {
+            get
+            {
+                return _map;
+            }
+        }
+
+        [DataMember(Name = "mode")]
+        private string _mode;
+
+        public string Mode
+        {
+            get
+            {
+                return _mode;
+            }
+        }
+
+        [DataMember(Name = "priority")]
+        private bool _priority;
+
+        public bool Priority
+        {
+            get
+            {
+                return _priority;
+            }
+        }
+
+        [DataMember(Name = "title")]
+        private string _title;
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+        }
+
+        [DataMember(Name = "type")]
+        private string _type;
+
+        public string Type
+        {
+            get
+            {
+                return _type;
+            }
+        }
+    }
+}
